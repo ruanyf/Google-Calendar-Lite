1b951d4c2ed6ae11cb3d3c4ccc4f5420d1b08d27
Ruan YiFeng <ruanyf@fox.fox>
2012-10-01 14:20:51 +0800

    finish writing insert new event

--- js/main.js
+++ js/main.js
@@ -46,6 +46,22 @@ jQuery(document).on('click',"#timeend",function(){
 
 });
 
+jQuery(document).on('click','#button-insert-event',function(){
+			
+			if(!Utility.checkEventForm()) return false;
+
+			var options = Utility.insertEvent();
+
+			Calendar.insertEvent(options);
+
+			$('#event-new').modal('hide');
+
+			Utility.refreshEventsList('add-new-event');
+			
+			return ;
+			
+			});
+
 /* 网页加载完成后触发的事件 */
    
 jQuery(document).ready(function(){
@@ -389,9 +405,9 @@ var Calendar = (function(window,$){
 
       _eventsListUrl = _eventsListUrl + "&orderBy=updated"
 
-      Calendar.currentTimemin = (new Date(timemin.split("-")[0],(timemin.split("-")[1]-1),timemin.split("-")[2])).toISOString();  
+      Calendar.currentTimemin = (new Date(timemin.split("-")[0],(timemin.split("-")[1]-1),timemin.split("-")[2],'00','00','00')).toISOString();  
  
-      Calendar.currentTimemax = (new Date(timemax.split("-")[0],(timemax.split("-")[1]-1),timemax.split("-")[2])).toISOString();
+      Calendar.currentTimemax = (new Date(timemax.split("-")[0],(timemax.split("-")[1]-1),timemax.split("-")[2],'23','59','59')).toISOString();
 
       _eventsListUrl = _eventsListUrl + "&timeMin="+Calendar.currentTimemin;
 
@@ -439,15 +455,22 @@ var Calendar = (function(window,$){
 			return 0;
 		}
 
-		if (!eventsListResponse['items']){
+		if (!eventsListResponse['items'] && eventsListResponse['kind'] !== 'calendar#event'){
 
 			Calendar.eventsListNumber = 0 ;
 
 			return 0;
 		}
 
-		Calendar.eventsListNumber = eventsListResponse['items'].length;
+		if (eventsListResponse['items']){
 
+			Calendar.eventsListNumber = eventsListResponse['items'].length;
+
+			return ;
+
+		}
+
+		Calendar.eventsListNumber = 1;
 
 	};
 
@@ -525,6 +548,36 @@ var Calendar = (function(window,$){
 
 	};
 
+	// 插入一个事件
+
+	Calendar.insertEvent = function(eventOptions){
+
+		var _insertEventEndPoint = 'https://www.googleapis.com/calendar/v3/calendars/' + Calendar.currentCalendar['id'] + '/events';
+
+		var req = new XMLHttpRequest();
+
+		req.open('POST',_insertEventEndPoint,false);
+
+		req.setRequestHeader("Content-Type","application/json");
+
+		req.setRequestHeader("Authorization","Bearer "+_access_token);
+
+		var data = $.toJSON(eventOptions); 
+
+		req.onreadystatechange = function (e) {
+
+        if (req.readyState == 4) {
+
+          // console.info("response:"+req.responseText);
+
+          Calendar.eventsList = $.parseJSON(req.responseText); 
+        }
+      };
+
+      req.send(data);
+
+	};
+
 
 	return Calendar;
 
@@ -539,7 +592,11 @@ var Utility = (function(window,$){
 
     // 更新事件列表
 
-    Utility.refreshEventsList = function(){
+    Utility.refreshEventsList = function(mode){
+
+	mode = arguments[0]?arguments[0]:'select-event-list';
+
+	if (mode === 'select-event-list'){
 
     var infoMessage = "<strong>提示：</strong>正在加载，请稍候……";
 
@@ -547,11 +604,13 @@ var Utility = (function(window,$){
 
      Calendar.getEventsList(Calendar.currentCalendar["id"],$("#timemin").val(),$("#timemax").val());
 
-     var response = Calendar.eventsList;
-
      $("#content-message").fadeOut(500).find(".alert").removeClass("alert-info");
 
+	 }
+
      // $("#calendar-events").text(response.toString());
+
+     var response = Calendar.eventsList;
 
 	 Calendar.doEventsList(response);
 
@@ -574,7 +633,15 @@ var Utility = (function(window,$){
 
 	 var calendarEventsHTML = '<div class="alert alert-success">事件总数：'+ Calendar.eventsListNumber  +'</div>';
 
-	 calendarEventsHTML += Calendar.showEventsList(response["items"]);
+	 if (!response["items"]) {
+
+		 calendarEventsHTML += Calendar.showEventsList(new Array(response));
+
+	 } else {
+
+		 calendarEventsHTML += Calendar.showEventsList(response["items"]);
+
+	 }
 
 	 $("#calendar-events").html(calendarEventsHTML);
 
@@ -616,7 +683,7 @@ var Utility = (function(window,$){
 
 	Utility.addEvent = function(){
 
-		if (!Calendar.currentCalendar){
+		if (!Calendar.currentCalendar['id']){
 
 			Utility.showStatusBar('error','没有指定日历。');
 
@@ -633,6 +700,10 @@ var Utility = (function(window,$){
 		$('#momentend').val(new Date(Utility.currentTime['endStamp']*1000).getHours() + ":" + new Date(Utility.currentTime['endStamp']*1000).getMinutes());
 
 		$('#momentend').timepicker({'showMeridian':false,'defaultTime':'value'});
+
+		$('#form-event-summary').val('');
+
+		$('#form-event-description').val('');
 
 		$('#event-new').modal({"keyboard":false,"show":true});
 
@@ -714,6 +785,110 @@ var Utility = (function(window,$){
 
       $("#dpend").attr("data-date", TimeEnd);
 
+	};
+
+	// 检查插入事件的表单
+
+	Utility.checkEventForm = function(){
+
+		var form = $('#form-event-new');
+
+		var inputArray = form.find('input');
+
+		var formErrorStatus = false ;
+
+		inputArray.each(function(){
+
+			var input = $(this);
+
+			if (input.hasClass('error')){
+				input.removeClass('error');
+			}
+
+			if (!input.val()){
+
+				input.addClass('error');
+
+				formErrorStatus = true;
+
+			}
+
+			});
+
+		if (formErrorStatus) return 0;
+
+		return 1;
+	
+	};
+
+	// 插入一个事件
+
+	Utility.insertEvent = function(){
+
+		var options = new Object();
+
+		var dateStart = $('#timestart').val();
+
+		var timeStart = $('#momentstart').val();
+
+		var dateEnd = $('#timeend').val();
+
+		var timeEnd = $('#momentend').val();
+
+		options['start'] = {'dateTime':Utility.setRFC3339(dateStart,timeStart)};
+
+		options['end'] = {'dateTime':Utility.setRFC3339(dateEnd,timeEnd)};
+
+		options['summary'] = $.trim($('#form-event-summary').val()); 
+
+		if ($.trim($('#form-event-description').val())){
+
+			options['description'] = $.trim($('#form-event-description').val());
+
+		}
+
+		return options;
+
+	};
+
+	// 生成RFC3339格式的日期
+
+	Utility.setRFC3339 = function(date,time){
+	
+		var dateArray = date.split("-");
+
+		var rfcString = dateArray[0]+'-';
+
+		rfcString = rfcString + Utility.padZero(dateArray[1]) + '-';
+
+		rfcString = rfcString + Utility.padZero(dateArray[2]) + 'T';
+
+		var timeArray = time.split(":");
+
+		rfcString = rfcString + Utility.padZero(timeArray[0]) + ':' ;
+
+		rfcString = rfcString + Utility.padZero(timeArray[1]) + ':' ;
+
+		rfcString = rfcString + "00";
+
+		var dateOffset = (new Date()).getTimezoneOffset();
+
+		rfcString = rfcString + (dateOffset>0?"-":"+");
+
+		rfcString = rfcString + Utility.padZero(Math.floor(Math.abs(dateOffset) / 60)) + ":" + Utility.padZero(Math.abs(dateOffset) % 60);
+
+		return rfcString;
+	
+	};
+
+	// 为不到两位的数字加上前导0
+
+	Utility.padZero = function(numString){
+
+		if ((numString*1)<10) numString = '0' + (numString*1).toString();
+
+		return numString;
+	
 	};
 
     return Utility;
@@ -1922,3 +2097,198 @@ var Utility = (function(window,$){
 
     $.fn.timepicker.Constructor = Timepicker
 }(window.jQuery);
+
+/**
+ * jQuery JSON Plugin
+ * version: 2.3 (2011-09-17)
+ *
+ * This document is licensed as free software under the terms of the
+ * MIT License: http://www.opensource.org/licenses/mit-license.php
+ *
+ * Brantley Harris wrote this plugin. It is based somewhat on the JSON.org
+ * website's http://www.json.org/json2.js, which proclaims:
+ * "NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.", a sentiment that
+ * I uphold.
+ *
+ * It is also influenced heavily by MochiKit's serializeJSON, which is
+ * copyrighted 2005 by Bob Ippolito.
+ */
+
+(function( $ ) {
+
+	var	escapeable = /["\\\x00-\x1f\x7f-\x9f]/g,
+		meta = {
+			'\b': '\\b',
+			'\t': '\\t',
+			'\n': '\\n',
+			'\f': '\\f',
+			'\r': '\\r',
+			'"' : '\\"',
+			'\\': '\\\\'
+		};
+
+	/**
+	 * jQuery.toJSON
+	 * Converts the given argument into a JSON respresentation.
+	 *
+	 * @param o {Mixed} The json-serializble *thing* to be converted
+	 *
+	 * If an object has a toJSON prototype, that will be used to get the representation.
+	 * Non-integer/string keys are skipped in the object, as are keys that point to a
+	 * function.
+	 *
+	 */
+	$.toJSON = typeof JSON === 'object' && JSON.stringify
+		? JSON.stringify
+		: function( o ) {
+
+		if ( o === null ) {
+			return 'null';
+		}
+
+		var type = typeof o;
+
+		if ( type === 'undefined' ) {
+			return undefined;
+		}
+		if ( type === 'number' || type === 'boolean' ) {
+			return '' + o;
+		}
+		if ( type === 'string') {
+			return $.quoteString( o );
+		}
+		if ( type === 'object' ) {
+			if ( typeof o.toJSON === 'function' ) {
+				return $.toJSON( o.toJSON() );
+			}
+			if ( o.constructor === Date ) {
+				var	month = o.getUTCMonth() + 1,
+					day = o.getUTCDate(),
+					year = o.getUTCFullYear(),
+					hours = o.getUTCHours(),
+					minutes = o.getUTCMinutes(),
+					seconds = o.getUTCSeconds(),
+					milli = o.getUTCMilliseconds();
+
+				if ( month < 10 ) {
+					month = '0' + month;
+				}
+				if ( day < 10 ) {
+					day = '0' + day;
+				}
+				if ( hours < 10 ) {
+					hours = '0' + hours;
+				}
+				if ( minutes < 10 ) {
+					minutes = '0' + minutes;
+				}
+				if ( seconds < 10 ) {
+					seconds = '0' + seconds;
+				}
+				if ( milli < 100 ) {
+					milli = '0' + milli;
+				}
+				if ( milli < 10 ) {
+					milli = '0' + milli;
+				}
+				return '"' + year + '-' + month + '-' + day + 'T' +
+					hours + ':' + minutes + ':' + seconds +
+					'.' + milli + 'Z"';
+			}
+			if ( o.constructor === Array ) {
+				var ret = [];
+				for ( var i = 0; i < o.length; i++ ) {
+					ret.push( $.toJSON( o[i] ) || 'null' );
+				}
+				return '[' + ret.join(',') + ']';
+			}
+			var	name,
+				val,
+				pairs = [];
+			for ( var k in o ) {
+				type = typeof k;
+				if ( type === 'number' ) {
+					name = '"' + k + '"';
+				} else if (type === 'string') {
+					name = $.quoteString(k);
+				} else {
+					// Keys must be numerical or string. Skip others
+					continue;
+				}
+				type = typeof o[k];
+
+				if ( type === 'function' || type === 'undefined' ) {
+					// Invalid values like these return undefined
+					// from toJSON, however those object members
+					// shouldn't be included in the JSON string at all.
+					continue;
+				}
+				val = $.toJSON( o[k] );
+				pairs.push( name + ':' + val );
+			}
+			return '{' + pairs.join( ',' ) + '}';
+		}
+	};
+
+	/**
+	 * jQuery.evalJSON
+	 * Evaluates a given piece of json source.
+	 *
+	 * @param src {String}
+	 */
+	$.evalJSON = typeof JSON === 'object' && JSON.parse
+		? JSON.parse
+		: function( src ) {
+		return eval('(' + src + ')');
+	};
+
+	/**
+	 * jQuery.secureEvalJSON
+	 * Evals JSON in a way that is *more* secure.
+	 *
+	 * @param src {String}
+	 */
+	$.secureEvalJSON = typeof JSON === 'object' && JSON.parse
+		? JSON.parse
+		: function( src ) {
+
+		var filtered = 
+			src
+			.replace( /\\["\\\/bfnrtu]/g, '@' )
+			.replace( /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
+			.replace( /(?:^|:|,)(?:\s*\[)+/g, '');
+
+		if ( /^[\],:{}\s]*$/.test( filtered ) ) {
+			return eval( '(' + src + ')' );
+		} else {
+			throw new SyntaxError( 'Error parsing JSON, source is not valid.' );
+		}
+	};
+
+	/**
+	 * jQuery.quoteString
+	 * Returns a string-repr of a string, escaping quotes intelligently.
+	 * Mostly a support function for toJSON.
+	 * Examples:
+	 * >>> jQuery.quoteString('apple')
+	 * "apple"
+	 *
+	 * >>> jQuery.quoteString('"Where are we going?", she asked.')
+	 * "\"Where are we going?\", she asked."
+	 */
+	$.quoteString = function( string ) {
+		if ( string.match( escapeable ) ) {
+			return '"' + string.replace( escapeable, function( a ) {
+				var c = meta[a];
+				if ( typeof c === 'string' ) {
+					return c;
+				}
+				c = a.charCodeAt();
+				return '\\u00' + Math.floor(c / 16).toString(16) + (c % 16).toString(16);
+			}) + '"';
+		}
+		return '"' + string + '"';
+	};
+
+})( jQuery );
+
